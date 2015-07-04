222fac6d21fbe1142aa6f1e9d17e4603d1dda08c
Abel <abel@abelml.eu>
2015-07-04 19:15:12 +0200

    Uso de base de datos para almacenar preguntas y respuestas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,12 +1,21 @@
 var path = require('path');
+var models = require('../models/models.js');
+
+var dirname = path.resolve(path.dirname());
 
 // GET /quizes/question/
 exports.question = function (req, res) {
-	res.render(path.join(__dirname, '../views/quizes/question'), {pregunta: 'Capital de Italia'});
+	var route = path.join(dirname, 'views/quizes/question');
+	models.Quiz.findAll().success(function (quiz) {
+		res.render(route, {pregunta: quiz[0].pregunta});
+	});
 };
 
 // GET /quizes/answer/
 exports.answer = function (req, res) {
-	var result = req.query.respuesta === 'Roma' ? 'Correcto' : 'Incorrecto';
-	res.render(path.join(__dirname, '../views/quizes/answer'), {respuesta: result});
+	var route = path.join(dirname, 'views/quizes/answer');
+	models.Quiz.findAll().success(function (quiz) {
+		var result = req.query.respuesta === quiz[0].respuesta ? 'Correcto' : 'Incorrecto';
+		res.render(route, {respuesta: result});
+	});
 };
