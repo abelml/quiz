14f1ff3402f64ead692fa7ec6c3ed515e8642080
Abel <abel@abelml.eu>
2015-07-19 11:18:28 +0200

    Autorizacion de acceso a operaciones

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -5,7 +5,7 @@ var dirname = path.resolve(path.dirname());
 
 // Autoload
 exports.load = function (req, res, next, quizId) {
-	var route = path.join(dirname, 'views/quizes/index.js');
+	var route = path.join(dirname, 'views/quizes/index.ejs');
 	var clause = {
 		where: {
 			id: Number(quizId)
@@ -27,8 +27,8 @@ exports.load = function (req, res, next, quizId) {
 };
 
 // GET /quizes
-exports.index = function (req, res) {
-	var route = path.join(dirname, 'views/quizes/index.js');
+exports.index = function (req, res, next) {
+	var route = path.join(dirname, 'views/quizes/index.ejs');
 	var params = {};
 	if (req.query.search) {
 		var textToFind = '%' + req.query.search.replace(/\s+/g, '%') + '%'
@@ -43,13 +43,13 @@ exports.index = function (req, res) {
 
 // GET /quizes/question/
 exports.show = function (req, res) {
-	var route = path.join(dirname, 'views/quizes/show.js');
+	var route = path.join(dirname, 'views/quizes/show.ejs');
 	res.render(route, {quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/answer/
 exports.answer = function (req, res) {
-	var route = path.join(dirname, 'views/quizes/answer');
+	var route = path.join(dirname, 'views/quizes/answer.ejs');
 	var result = req.query.respuesta === req.quiz.respuesta ? 'Correcto' : 'Incorrecto';
 	res.render(route, {quiz: req.quiz, result: result, errors: []});
 };
@@ -68,12 +68,12 @@ exports.new = function (req, res) {
 // POST /quizes/create
 exports.create = function (req, res) {
 	var quiz = models.Quiz.build(req.body.quiz);
-	saveQuestion(quiz, 'views/quizes/new', res);
+	saveQuestion(quiz, 'views/quizes/new.ejs', res);
 };
 
 // GET /quizes/:id/edit
 exports.edit = function (req, res) {
-	var route = path.join(dirname, 'views/quizes/edit');
+	var route = path.join(dirname, 'views/quizes/edit.ejs');
 	var quiz = req.quiz;
 	res.render(route, {quiz: quiz, errors: []});
 };
@@ -83,11 +83,11 @@ exports.update = function (req, res) {
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
 	req.quiz.tema = req.body.quiz.tema;
-	saveQuestion(req.quiz, 'views/quizes/edit', res);
+	saveQuestion(req.quiz, 'views/quizes/edit.ejs', res);
 };
 
 // DELETE /quizes/:id
-exports.destroy = function (req, res) {
+exports.destroy = function (req, res, next) {
 	req.quiz.destroy().then(function () {
 		res.redirect('/quizes');
 	}).catch(function (err) {
